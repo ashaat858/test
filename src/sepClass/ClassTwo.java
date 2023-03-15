2dbaadf325cfde25218461edf9e3fffe4d7fe7ed
ashaat858 <123007281+ashaat858@users.noreply.github.com>
2023-03-15 13:13:00 -0500

    ok

---
+++ src/sepClass/ClassTwo.java
@@ -0,0 +1,38 @@
+package sepClass;
+
+public class ClassTwo {
+
+	public static void main(String[] args) {
+		
+////		DataType variableName = Value ;
+//		int x,y,z;
+//		x=10;
+//		y=20;
+//		z=30;
+////	int a=10;
+////	double b=10.50;
+////	char c= 'a';
+////	String d= "as";
+////	boolean	e= true;
+//	
+//////	a + b
+////	
+////	System.out.println(a+b);
+////	System.out.println(b-a);	
+////	System.out.println(a+c);
+////	System.out.println(d+b);
+////	System.out.println(a+b+d);
+//	
+//	System.out.println(x);
+//		x=40;
+//				System.out.println(x);	
+		
+		DataType variableName = value ;
+		int x,y,z;
+		x=10
+		y=30
+		syso
+		
+	}
+
+}
---
+++ src/sepClass/UkClass.java
@@ -0,0 +1,18 @@
+package sepClass;
+
+public class UkClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
